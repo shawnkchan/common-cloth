84ae093bf0363a6724785abae23459b01ea9676b
shawn <shawnkengkiat@gmail.com>
2024-05-30 16:11:08 +0800

    initial commit

---
+++ src/app/blog-entry/blog-entry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-entry',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-entry.component.html',
+  styleUrl: './blog-entry.component.css'
+})
+export class BlogEntryComponent {
+
+}
