e91c753aee47226a76e0606a6d33d6d5f8fdba6e
shawn <shawnkengkiat@gmail.com>
2024-05-31 12:26:54 +0800

    adds styling for home page with flex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlogEntryComponent } from './blog-entry/blog-entry.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BlogEntryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
