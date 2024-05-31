ab7420d210cabf2447a89ffcd90e3bc39fa155ec
shawn <shawnkengkiat@gmail.com>
2024-05-31 18:51:25 +0800

    feat: adds animation for transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BlogEntryComponent } from './blog-entry/blog-entry.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BlogEntryComponent],
+  imports: [RouterOutlet, BlogEntryComponent,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('slideUpDown', [
+      state('visible', style({ transform: 'translateY(0)' })),
+      state('hidden', style({ transform: 'translateY(-100%)' })),
+      transition('visible <=> hidden', [
+        animate('0.3s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'common-cloth';
+  isHidden = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollThreshold = 200;
+    this.isHidden = window.scrollY > scrollThreshold;
+  }
 }
